e4a06b4c1c1af0120563dc17ae4fb4076cb08f02
jbshin <josephshin93@gmail.com>
2017-02-09 23:23:05 -0500

    created script for rates/tours pages, has code for the functionality of the subsections/examples of the tours and special events, and has code of the mapbox implementation (was created just to get an idea and Mapbox API key from city-data-visualizer project is being used) --- THIS IS STILL IN DEVELOPMENT, PROPER FUNCTIONALITY TO NEW LAYOUT WILL BE IMPLEMENTED

---
+++ js/rates.js
@@ -0,0 +1,70 @@
+//rates.js
+
+$("select").on("change", function () {
+	"use strict";
+	var sectionChar = this.value.substring(0, 1),
+		selectedValue = this.value.substring(1),
+		section;
+
+	if (sectionChar === "T") {
+		section = "tours";
+	} else if (sectionChar === "S") {
+		section = "special";
+	}
+	var exampleId = "#" + section + "_" + selectedValue,
+		options =  "#" + section + "_dropdown option" 
+
+	$(options).each(function () {
+		"use strict";
+		var nonSelectValue = this.value.substring(1);
+		if (nonSelectValue !== selectedValue) {
+			var nonSelectId = "#" + section + "_" + nonSelectValue;
+			$(nonSelectId).css("display", "none");
+		}
+	});
+	$(exampleId).css("display", "block");
+});
+
+
+
+// var weekiWacheeCoordinates = [28.5156, -82.5729];
+var weekiWacheeCoordinates = [28.526, -82.615];
+// var mymap = L.map('services_overview_visualization').setView(weekiWacheeCoordinates, 14);
+
+function getMapboxApiKey() {
+    "use strict";
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080",
+        dataType: "text",
+        crossDomain: true,
+        success: function (data) {
+            L.mapbox.accessToken = data;
+            L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=' + data, {
+                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                accessToken: data
+            }).addTo(mymap);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log(xhr.status);
+            console.log(thrownError);
+        }
+    });
+}
+// getMapboxApiKey();
+
+function plotPoints() {
+	var wwKayakingMarker = L.marker([28.529652, -82.5898919]).addTo(mymap);
+	wwKayakingMarker.bindPopup("Weeki Wachee Kayaking");
+	var hospitalHoleMarker = L.marker([28.530534, -82.623217]).addTo(mymap);
+	hospitalHoleMarker.bindPopup("Hospital Hole");
+	var wwStateParkMarker = L.marker([28.517092, -82.574626]).addTo(mymap);
+	wwStateParkMarker.bindPopup("Weeki Wachee<br>State Park");
+	var wwChristianCamp = L.marker([28.534618, -82.617414]).addTo(mymap);
+	wwChristianCamp.bindPopup("Weeki Wachee<br>Christian Camp");
+	var tailGaters = L.marker([28.529066, -82.5893509]).addTo(mymap);
+	tailGaters.bindPopup("Tail Gaters");	
+}
+
+
+
