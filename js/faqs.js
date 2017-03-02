16b601f3ae104fd6ed40852868ba6fc8abda2692
BenDaG <benbrewerbowman@gmail.com>
2017-03-01 19:49:52 -0500

    Skeleton added, faqs html+css+js added

--- js/faqs.js
+++ js/faqs.js
@@ -3,7 +3,22 @@
 
 /*-----------------------------------------------------------------------------*/
 /* Ben Code */
+ $(document).ready(function() {
 
+    $('.faq_question').click(function() {
+
+        if ($(this).parent().is('.open')){
+            $(this).closest('.faq').find('.faq_answer_container').animate({'height':'0'},250);
+            $(this).closest('.faq').removeClass('open');
+
+        }
+        else{
+            var newHeight =$(this).closest('.faq').find('.faq_answer').height() +'px';
+            $(this).closest('.faq').find('.faq_answer_container').animate({'height':newHeight},250);
+            $(this).closest('.faq').addClass('open');
+        }
+    });
+});
 /* Ben Code END */
 /*-----------------------------------------------------------------------------*/
 
