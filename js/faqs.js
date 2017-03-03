36ad6b61783752f7146acc0c46976b1cf114c7a6
BenDaG <benbrewerbowman@gmail.com>
2017-03-03 00:01:21 -0500

    Index wrapped in skeleton container

--- js/faqs.js
+++ js/faqs.js
@@ -1,16 +1,17 @@
 // faqs.js
 
-
 /*-----------------------------------------------------------------------------*/
 /* Ben Code */
+
+//Show and hide answer when FAQ question clicked
  $(document).ready(function() {
-
     $('.faq_question').click(function() {
-
+        //If answer is open, hide
         if ($(this).parent().is('.open')){
             $(this).closest('.faq').find('.faq_answer_container').animate({'height':'0'},250);
             $(this).closest('.faq').removeClass('open');
         }
+        //If answer is closed, show
         else{
             var newHeight =$(this).closest('.faq').find('.faq_answer').height() +'px';
             $(this).closest('.faq').find('.faq_answer_container').animate({'height':newHeight},250);
